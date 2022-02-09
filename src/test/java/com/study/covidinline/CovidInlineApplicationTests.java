78419c45c1234c2506f477117e6c3dc0b87f2f1b
BangDDong <dhbang@kakao.com>
2022-02-09 17:53:19 +0900

    Init spring-boot

---
+++ src/test/java/com/study/covidinline/CovidInlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.covidinline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidInlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
