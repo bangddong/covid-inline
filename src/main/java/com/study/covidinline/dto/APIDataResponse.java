259d33e26c797a1986c131c515c3cc841dc612e1
BangDDong <dhbang@kakao.com>
2022-02-10 22:05:27 +0900

    Update #1 - api data response
    type safety를 위해 제너릭 추가
    성공을 가정한 response로 불필요 파라미터 제거

--- src/main/java/com/study/covidinline/dto/APIDataResponse.java
+++ src/main/java/com/study/covidinline/dto/APIDataResponse.java
@@ -1,22 +1,26 @@
 package com.study.covidinline.dto;
 
+import com.study.covidinline.constant.ErrorCode;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
+/**
+ * 공통 API response
+ */
 @Getter
 @ToString
 @EqualsAndHashCode(callSuper = true)
-public class APIDataResponse extends APIErrorResponse{
+public class APIDataResponse<T> extends APIErrorResponse{
 
-    private final Object data;
+    private final T data;
 
-    private APIDataResponse(boolean success, Integer errorCode, String message, Object data) {
-        super(success, errorCode, message);
+    private APIDataResponse(T data) {
+        super(true, ErrorCode.OK.getCode(), ErrorCode.OK.getMessage());
         this.data = data;
     }
 
-    public static APIDataResponse of(boolean success, Integer errorCode, String message, Object data) {
-        return new APIDataResponse(success, errorCode, message, data);
+    public static <T> APIDataResponse<T> of(T data) {
+        return new APIDataResponse<T>(data);
     }
 }
