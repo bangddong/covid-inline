9cdc863774a4dfa932046aac7dcf0a954d32a9b4
BangDDong <gusehdqkd@naver.com>
2022-02-11 11:57:31 +0900

    Add #1 - api controller test
    api test 파일 작성

---
+++ src/test/java/com/study/covidinline/controller/error/BaseErrorControllerTest.java
@@ -0,0 +1,33 @@
+package com.study.covidinline.controller.error;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BaseErrorController.class)
+class BaseErrorControllerTest {
+
+    private final MockMvc mvc;
+
+    public BaseErrorControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view][GET] 에러 페이지 요청 - 페이지 없음")
+    @Test
+    void giveNothing_whenRequestingPage_thenReturns404ErrorPage() throws Exception{
+        //given
+
+
+        //when & then
+        mvc.perform(get("/wrong-rui"))
+                .andExpect(status().isNotFound())
+                .andDo(print());
+    }
+}
