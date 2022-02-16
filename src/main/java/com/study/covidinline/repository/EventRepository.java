b837573b0f31a7ff8349b9da24fa4427903b2687
BangDDong <gusehdqkd@naver.com>
2022-02-16 21:20:35 +0900

    Add #4 - Event Service & test
    * 'RepositoryConfig' : 'EventRepository' 빈 등록을 위해 추가
    * 'EventRepository' : 데이터 제공용 인터페이스로 작성
        * 인스턴스 생성 편의를 위해 default 사용하였으나 삭제 필요

---
+++ src/main/java/com/study/covidinline/repository/EventRepository.java
@@ -0,0 +1,40 @@
+package com.study.covidinline.repository;
+
+import com.study.covidinline.constant.EventStatus;
+import com.study.covidinline.dto.EventDTO;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+// TODO: 인스턴스 생성 편의를 위해 임시로 default 사용.
+// TODO: repository layer 구현시 삭제예정
+public interface EventRepository {
+
+    default List<EventDTO> findEvents(
+            Long placeId,
+            String eventName,
+            EventStatus eventStatus,
+            LocalDateTime eventStartDatetime,
+            LocalDateTime eventEndDatetime
+    ) {
+        return List.of();
+    }
+
+    default Optional<EventDTO> findEvent(Long eventId) {
+        return Optional.empty();
+    }
+
+    default boolean insertEvent(EventDTO eventDTO) {
+        return false;
+    }
+
+    default boolean updateEvent(Long eventId, EventDTO eventDTO) {
+        return false;
+    }
+
+    default boolean deleteEvent(Long eventId) {
+        return false;
+    }
+
+}
