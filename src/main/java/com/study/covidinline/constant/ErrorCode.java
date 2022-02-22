4e78b9bb5e88777b2f7896ed771ce3c1e05099d5
BangDDong <gusehdqkd@naver.com>
2022-02-22 16:45:15 +0900

    Add - ErrorCode : not fount
    에러코드 추가

--- src/main/java/com/study/covidinline/constant/ErrorCode.java
+++ src/main/java/com/study/covidinline/constant/ErrorCode.java
@@ -21,6 +21,7 @@ public enum ErrorCode {
     BAD_REQUEST(10000, ErrorCategory.CLIENT_SIDE, "Bad request"),
     SPRING_BAD_REQUEST(10001, ErrorCategory.CLIENT_SIDE, "Spring-detected bad request"),
     VALIDATION_ERROR(10002, ErrorCategory.CLIENT_SIDE, "Validation error"),
+    NOT_FOUND(1000, ErrorCategory.CLIENT_SIDE, "Requested resource is not found"),
 
     // server error
     INTERNAL_ERROR(20000, ErrorCategory.SERVER_SIDE, "Internal error"),
