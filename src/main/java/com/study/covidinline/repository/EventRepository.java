d3efe941cbc62c325ea1c64c0a37f0e90efbd8a5
BangDDong <gusehdqkd@naver.com>
2022-02-22 16:39:29 +0900

    Add #8 - repository
    각 도메인에 사용할 레포지토리 인터페이스 작성

--- src/main/java/com/study/covidinline/repository/EventRepository.java
+++ src/main/java/com/study/covidinline/repository/EventRepository.java
@@ -1,7 +1,9 @@
 package com.study.covidinline.repository;
 
 import com.study.covidinline.constant.EventStatus;
+import com.study.covidinline.domain.Event;
 import com.study.covidinline.dto.EventDTO;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -9,7 +11,7 @@ import java.util.Optional;
 
 // TODO: 인스턴스 생성 편의를 위해 임시로 default 사용.
 // TODO: repository layer 구현시 삭제예정
-public interface EventRepository {
+public interface EventRepository extends JpaRepository<Event, Long> {
 
     default List<EventDTO> findEvents(
             Long placeId,
