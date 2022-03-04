093737f21634c7fc7e0e6c38e7cd0449a664b615
BangDDong <gusehdqkd@naver.com>
2022-03-04 17:44:55 +0900

    Update - ErrorCode to have HttpStatus
    http status code를 좀 더 편하게 관리하기 위해 ErrorCode 안 ErrorCategory를 삭제 및 HttpStatus 사용

--- src/main/java/com/study/covidinline/constant/ErrorCode.java
+++ src/main/java/com/study/covidinline/constant/ErrorCode.java
@@ -1,8 +1,11 @@
 package com.study.covidinline.constant;
 
+import com.study.covidinline.exception.GeneralException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
+import java.util.Arrays;
 import java.util.Optional;
 import java.util.function.Predicate;
 
@@ -15,23 +18,36 @@ import java.util.function.Predicate;
 public enum ErrorCode {
 
     // success case
-    OK(0, ErrorCategory.NORMAL, "OK"),
+    OK(0, HttpStatus.OK, "OK"),
 
     // client error
-    BAD_REQUEST(10000, ErrorCategory.CLIENT_SIDE, "Bad request"),
-    SPRING_BAD_REQUEST(10001, ErrorCategory.CLIENT_SIDE, "Spring-detected bad request"),
-    VALIDATION_ERROR(10002, ErrorCategory.CLIENT_SIDE, "Validation error"),
-    NOT_FOUND(1000, ErrorCategory.CLIENT_SIDE, "Requested resource is not found"),
+    BAD_REQUEST(10000, HttpStatus.BAD_REQUEST, "Bad request"),
+    SPRING_BAD_REQUEST(10001, HttpStatus.BAD_REQUEST, "Spring-detected bad request"),
+    VALIDATION_ERROR(10002, HttpStatus.BAD_REQUEST, "Validation error"),
+    NOT_FOUND(10003, HttpStatus.BAD_REQUEST, "Requested resource is not found"),
 
     // server error
-    INTERNAL_ERROR(20000, ErrorCategory.SERVER_SIDE, "Internal error"),
-    SPRING_INTERNAL_ERROR(20001, ErrorCategory.SERVER_SIDE, "Spring-detected internal error"),
-    DATA_ACCESS_ERROR(20002, ErrorCategory.SERVER_SIDE, "Data access error")
+    INTERNAL_ERROR(20000, HttpStatus.INTERNAL_SERVER_ERROR, "Internal error"),
+    SPRING_INTERNAL_ERROR(20001, HttpStatus.INTERNAL_SERVER_ERROR, "Spring-detected internal error"),
+    DATA_ACCESS_ERROR(20002, HttpStatus.INTERNAL_SERVER_ERROR, "Data access error")
     ;
 
     private final Integer code;
-    private final ErrorCategory errorCategory;
+    private final HttpStatus httpStatus;
     private final String message;
+
+    public static ErrorCode valueOf(HttpStatus httpStatus) {
+        if (httpStatus == null) throw new GeneralException("HttpStatus is null.");
+
+        return Arrays.stream(values())
+                .filter(errorCode -> errorCode.getHttpStatus() == httpStatus)
+                .findFirst()
+                .orElseGet(() -> {
+                    if (httpStatus.is4xxClientError()) return ErrorCode.BAD_REQUEST;
+                    else if (httpStatus.is5xxServerError()) return ErrorCode.INTERNAL_ERROR;
+                    else return ErrorCode.OK;
+                });
+    }
 
     // basic exception message
     public String getMessage(Throwable e) {
@@ -45,21 +61,9 @@ public enum ErrorCode {
                 .orElse(getMessage());
     }
 
-    public boolean isClientSideError() {
-        return this.getErrorCategory() == ErrorCategory.CLIENT_SIDE;
-    }
-
-    public boolean isServerSideError() {
-        return this.getErrorCategory() == ErrorCategory.SERVER_SIDE;
-    }
-
     @Override
     public String toString() {
         return String.format("%s (%d)", name(), this.getCode());
-    }
-
-    public enum ErrorCategory {
-        NORMAL, CLIENT_SIDE, SERVER_SIDE
     }
 
 }
