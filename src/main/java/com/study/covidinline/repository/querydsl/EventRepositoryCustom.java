ba885c1509e649118cc8c1890e9a40e0b9ace943
BangDDong <gusehdqkd@naver.com>
2022-03-08 20:56:34 +0900

    Implement #11 - EventRepositoryCustom using
    Querydsl 실습.
    * `EventRepositoryCustomImpl`로 querydsl 구현체 작성하여 다이나믹 쿼리 실습

---
+++ src/main/java/com/study/covidinline/repository/querydsl/EventRepositoryCustom.java
@@ -0,0 +1,19 @@
+package com.study.covidinline.repository.querydsl;
+
+import com.study.covidinline.constant.EventStatus;
+import com.study.covidinline.dto.EventViewResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDateTime;
+
+public interface EventRepositoryCustom {
+    Page<EventViewResponse> findEventViewPageBySearchParams(
+            String placeName,
+            String eventName,
+            EventStatus eventStatus,
+            LocalDateTime eventStartDatetime,
+            LocalDateTime eventEndDatetime,
+            Pageable pageable
+    );
+}
